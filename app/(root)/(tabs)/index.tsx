054c02427d1c98dc7e966a90337f22aac029a4a3
Jepoy <poyhidalgo@gmail.com>
2025-06-06 18:29:30 +0800

    feat: integrate NativeWind and TailwindCSS for styling
    - Added nativewind-env.d.ts for NativeWind type definitions.
    - Updated package-lock.json and package.json to include NativeWind and TailwindCSS dependencies.
    - Changed Expo scripts for Android and iOS to use `expo run` commands.
    - Created tailwind.config.js for TailwindCSS configuration.
    - Updated tsconfig.json to include nativewind-env.d.ts for TypeScript support.

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -10,6 +10,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
+      <Text className="font-bold text-lg my-10">Welcome to ADA Cafe</Text>
       <Link href={'/login'}>Login</Link>
       <Link href={'/(root)/(tabs)/profile'}>Profile</Link>
     </View>
